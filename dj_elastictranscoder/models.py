27ce637b6c2f53de03c802bf9ded9832a7059007
Aashish Sangle <crazyscorpio12@gmail.com>
2018-03-22 13:48:15 +0530

    fix for django 1.11

--- dj_elastictranscoder/models.py
+++ dj_elastictranscoder/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 import django
-if django.VERSION > (1, 8, 19, 'final', 0):
-    from django.contrib.contenttypes.fields import GenericForeignKey
-else:
-    from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class EncodeJob(models.Model):
